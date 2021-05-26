7a474c138335eff4c00a8c9bc0de8d08b9912398
Natanael Nogueira <natanael.nogueiraa@gmail.com>
2021-05-25 22:35:26 -0300

    Adicionado classe Operacoes e Metodos Calculadora

--- CalculadoraConsole/CalculadoraConsole/Program.cs
+++ CalculadoraConsole/CalculadoraConsole/Program.cs
@@ -10,25 +10,24 @@ namespace CalculadoraConsole
     {
         static void Main(string[] args)
         {
-
             int n1;
             int n2;
             string escolha;
 
+            Console.Write(Operacoes.Soma(10,50));
+            Console.Write(Operacoes.Soma(5, 5));
 
             Console.Write("Digite um numero:");
             n1 = Convert.ToInt32(Console.ReadLine());
 
-
             Console.Write("Digite outro numero:");
             n2 = Convert.ToInt32(Console.ReadLine());
 
-
             Console.WriteLine("Escolha um operação:");
-            Console.WriteLine("A soma");
-            Console.WriteLine("B subtração");
-            Console.WriteLine("C multiplicação");
-            Console.WriteLine("D divisão");
+            Console.WriteLine("A - Soma");
+            Console.WriteLine("B - Subtração");
+            Console.WriteLine("C - Multiplicação");
+            Console.WriteLine("D - Divisão");
             Console.WriteLine("");
 
             escolha = Console.ReadLine();
@@ -39,31 +38,27 @@ namespace CalculadoraConsole
                 switch (escolha.ToUpper())
                 {
                     case "A":
-                        Console.WriteLine($"A soma dos dois numeros é : { n1 + n2}");
+                        Console.WriteLine($"A Soma dos dois numeros é : { Operacoes.Soma(n1,n2) }");
                         break;
                     case "B":
-                        Console.WriteLine($"A soma dos dois numeros é : { n1 - n2}");
+                        Console.WriteLine($"A Subtração dos dois numeros é : { Operacoes.Subtracao(n1,n2) }");
                         break;
                     case "C":
-                        Console.WriteLine($"A soma dos dois numeros é : { n1 * n2}");
+                        Console.WriteLine($"A Multiplicação dos dois numeros é : { Operacoes.Multiplicacao(n1, n2)}");
                         break;
                     case "D":
-                        Console.WriteLine($"A soma dos dois numeros é : { n1 / n2}");
+                        Console.WriteLine($"A Divisão dos dois numeros é : { Operacoes.Divisao(n1, n2) }");
                         break;
 
                     default:
                         Console.WriteLine("FATAL ERROR! : Você escolheu uma opção invalida! Tente novamente");
                         break;
-
-
                 }
             }
             else
             {
                 Console.WriteLine("ERROR.");
             }
-
-
 
             Console.WriteLine("");
             Console.WriteLine("Digite uma tecla para sair.");
